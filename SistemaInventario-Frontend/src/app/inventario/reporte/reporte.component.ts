8f3fdd5e4250979647d6ba1d6a7b48e2fc0a5b3c
Danniel2002 <57758008+Danniel2002@users.noreply.github.com>
2021-12-01 14:25:07 -0500

    PDF Rportes

--- SistemaInventario-Frontend/src/app/inventario/reporte/reporte.component.ts
+++ SistemaInventario-Frontend/src/app/inventario/reporte/reporte.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Reporte } from 'src/app/modelo/Reporte';
 import { ReporteService } from 'src/app/Service/reportes.service';
+import pdfMake from 'pdfmake/build/pdfmake';
+import pdfFonts from 'pdfMake/build/vfs_fonts';
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 @Component({
   selector: 'app-reporte',
@@ -25,4 +28,39 @@ export class ReporteComponent implements OnInit {
 
   }
 
+  generarPDF(){
+
+    
+    var pdfDefinition ={
+      content: [
+       {
+         table: {
+           widths: [30, 70, 100, 60, 70, 60, 60],
+           alignment: "center",
+           body: [
+             [
+              'ID',
+              'FECHA',
+              'PRODUCTO',
+              'TIPO',
+              'CANTIDAD',
+              'PRECIO',
+              'TOTAL'
+             ],
+             [
+              this.reportes.map(row => [row.idReporte]),
+              this.reportes.map(row => [row.fecha]),
+              this.reportes.map(row => [row.nombre]),
+              this.reportes.map(row => [row.tipo]),
+              this.reportes.map(row => [row.cantidad]),
+              this.reportes.map(row => [row.precio]),
+              this.reportes.map(row => [row.total])
+             ]
+           ]
+         }
+       }
+      ]
+    }
+    pdfMake.createPdf(pdfDefinition).open();
+  }
 }
